86dd1357a7b7cf31099050375ff87eb6cac68d6e
KEERTHANADURAISAM <tarakeerthu@gmail.com>
2023-08-26 10:13:02 +0530

    new

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
+const bcrypt = require("bcryptjs");
+// const jwt = require("jwt");
 const mongodb = require("mongodb");
 const mongoClient = mongodb.MongoClient;
 const URL = process.env.DB;
@@ -12,72 +14,32 @@ app.get("/home", (req, res) => {
   res.json({ message: "welcome" });
 });
 
-app.post("/register", async (req, res) => {
-  try {
-    const connection = await mongoClient.connect(URL);
-    const db = connection.db(DB);
-    const user = await db.collection("userRegister").insertOne(req.body);
-    await connection.close();
-    res.status(200).json({ message: "register successfully" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "can't connect database" });
-  }
+
+
+app.post("/register", async function (req, res) {
+ try{
+  const connection = await mongodb.connect(URL);
+  const db = connection.db(DB);
+  const salt = await bcrypt.genSalt(10);
+  console.log(salt);
+  let hash = await bcrypt.hash(req.body.password, salt);
+  console.log(hash);
+  await db.collection("userRegister").insertOne(req.body);
+  connection.close();
+  res.json({ message: "register successfully" });
+ }catch(error){
+  console.log(error)
+  res.status(500).json({message:"something went wrong"})
+ }
 });
 
-app.put("/register/:id", async (req, res) => {
-  try {
-    const connection = await mongoClient.connect(URL);
-    const db = connection.db(DB);
-    const user = await db
-      .collection("userRegister")
-      .findOneAndUpdate(
-        { _id: new mongodb.ObjectId(req.params.id) },
-        { $set: req.body }
-      );
-    await connection.close();
-    res.status(200).json({ message: "Done" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "can't connect database" });
-  }
-});
 
-app.get("/register/:id", async (req, res) => {
-  try {
-    const connection = await mongoClient.connect(URL);
-    const db = connection.db(DB);
-    const user = await db
-      .collection("userRegister")
-      .findOne({ _id: new mongodb.ObjectId(req.params.id) });
-    await connection.close();
-    res.json(user);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "can't connect database" });
-  }
-});
 
 app.get("/users", async (req, res) => {
   try {
     const connection = await mongoClient.connect(URL);
     const db = connection.db(DB);
     const user = await db.collection("userRegister").find().toArray();
-    await connection.close();
-    res.json(user);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "can't connect database" });
-  }
-});
-
-app.delete("/register/:id", async (req, res) => {
-  try {
-    const connection = await mongoClient.connect(URL);
-    const db = connection.db(DB);
-    const user = await db
-      .collection("userRegister")
-      .findOneAndDelete({ _id: new mongodb.ObjectId(req.params.id) });
     await connection.close();
     res.json(user);
   } catch (error) {
