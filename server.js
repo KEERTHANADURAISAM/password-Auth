77646ba5d49079fa941fc290b6f8f3de1e75613e
KEERTHANADURAISAM <tarakeerthu@gmail.com>
2023-08-26 10:16:08 +0530

    new

--- server.js
+++ server.js
@@ -14,26 +14,22 @@ app.get("/home", (req, res) => {
   res.json({ message: "welcome" });
 });
 
-
-
 app.post("/register", async function (req, res) {
- try{
-  const connection = await mongodb.connect(URL);
-  const db = connection.db(DB);
-  const salt = await bcrypt.genSalt(10);
-  console.log(salt);
-  let hash = await bcrypt.hash(req.body.password, salt);
-  console.log(hash);
-  await db.collection("userRegister").insertOne(req.body);
-  connection.close();
-  res.json({ message: "register successfully" });
- }catch(error){
-  console.log(error)
-  res.status(500).json({message:"something went wrong"})
- }
+  try {
+    const connection = await mongodb.connect(URL);
+    const db = connection.db(DB);
+    const salt = await bcrypt.genSalt(10);
+    console.log(salt);
+    let hash = await bcrypt.hash(req.body.password, salt);
+    console.log(hash);
+    await db.collection("userRegister").insertOne(req.body);
+    connection.close();
+    res.json({ message: "register successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "something went wrong" });
+  }
 });
-
-
 
 app.get("/users", async (req, res) => {
   try {
